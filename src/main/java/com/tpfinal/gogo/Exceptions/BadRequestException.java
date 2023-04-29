f061fad9b99da8eee6d5d9007d552f5b102caef3
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-04-28 23:44:56 -0300

    manejo de excepciones

---
+++ src/main/java/com/tpfinal/gogo/Exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.tpfinal.gogo.Exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/tpfinal/gogo/Exceptions/InternalServerException.java
@@ -0,0 +1,7 @@
+package com.tpfinal.gogo.Exceptions;
+
+public class InternalServerException extends RuntimeException {
+    public InternalServerException(String message) {
+        super(message);
+    }
+}
