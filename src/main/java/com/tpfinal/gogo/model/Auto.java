1a2174bf419bea4343ace8a47586682f45240e28
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-05-24 20:31:40 -0300

    Creacion auto model

---
+++ src/main/java/com/tpfinal/gogo/model/Auto.java
@@ -0,0 +1,26 @@
+package com.tpfinal.gogo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Data
+@Entity
+public class Auto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "patente", nullable = false)
+    private String patente;
+    @Column(name = "color", nullable = false)
+    private String color;
+    @Column(name = "modelo", nullable = false)
+    private String modelo;
+    @Column(name = "marca", nullable = false)
+    private String marca;
+}
+
