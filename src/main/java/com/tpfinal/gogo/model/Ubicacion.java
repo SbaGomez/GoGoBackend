b5dfccfa5f3f0624ad5ee36a6efee8401fe6b0c8
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-05-30 00:10:37 -0300

    model ubicacion

---
+++ src/main/java/com/tpfinal/gogo/model/Ubicacion.java
@@ -0,0 +1,24 @@
+package com.tpfinal.gogo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Data
+@Entity
+public class Ubicacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+    @OneToOne(mappedBy = "inicio")
+    private Viaje viajeInicio;
+    @OneToOne(mappedBy = "destino")
+    private Viaje viajeDestino;
+}
+
