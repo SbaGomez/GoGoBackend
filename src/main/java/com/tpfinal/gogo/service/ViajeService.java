5ee8df7ded3dc0e357f16393c6ca495b0f8c07fd
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-05-30 00:10:59 -0300

    service viaje

---
+++ src/main/java/com/tpfinal/gogo/service/ViajeService.java
@@ -0,0 +1,74 @@
+package com.tpfinal.gogo.service;
+
+import com.tpfinal.gogo.model.*;
+import com.tpfinal.gogo.repository.ViajeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ViajeService {
+    private final ViajeRepository vr;
+
+    @Autowired
+    public ViajeService(ViajeRepository viajeRepository) {
+        this.vr = viajeRepository;
+    }
+
+    public void addViaje(Viaje v) {
+        vr.save(v);
+    }
+
+    public List<Viaje> getAll() {
+        return vr.findAll();
+    }
+
+    public Integer getTotal() {
+        return vr.findAll().size();
+    }
+
+    public Viaje updateViaje(Integer id, Viaje viaje) {
+        Viaje v = vr.findById(id).orElse(null);
+        if (v != null) {
+            if ((viaje.getHorarioSalida()) != null) {
+                v.setHorarioSalida(viaje.getHorarioSalida());
+            }
+            if ((viaje.getHorarioLlegada()) != null) {
+                v.setHorarioLlegada(viaje.getHorarioLlegada());
+            }
+            if ((viaje.getInicio()) != null) {
+                v.setInicio(viaje.getInicio());
+            }
+            if ((viaje.getDestino()) != null) {
+                v.setDestino(viaje.getDestino());
+            }
+            vr.save(v);
+        }
+        return v;
+    }
+
+    public boolean existsById(Integer id) {
+        return vr.existsById(id);
+    }
+
+    public void deleteViaje(Integer id) {
+        vr.deleteById(id);
+    }
+
+    public Viaje getViaje(Integer id) {
+        return vr.findById(id).orElse(null);
+    }
+
+    public Viaje findByUbicacionInicio(String inicio) {
+        return vr.findByUbicacionInicio(inicio);
+    }
+
+//    public Viaje findByUbicacionInicio(Ubicacion inicio) {
+//        return vr.findByUbicacionInicio(inicio.getNombre());
+//    }
+
+    public Viaje findByUbicacionDestino(String destino) {
+        return vr.findByUbicacionDestino(destino);
+    }
+}
