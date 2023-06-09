a7231fe038fed314960931f1c1ff165c408abe13
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-06-09 14:15:07 -0300

    relacion many to many usersxviajes

--- src/main/java/com/tpfinal/gogo/repository/ViajeRepository.java
+++ src/main/java/com/tpfinal/gogo/repository/ViajeRepository.java
@@ -2,10 +2,16 @@ package com.tpfinal.gogo.repository;
 
 import com.tpfinal.gogo.model.Viaje;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ViajeRepository extends JpaRepository<Viaje, Integer> {
-    Viaje findByInicio(String inicio);
-    Viaje findByDestino(String destino);
+/*    Viaje findByInicio(String inicio);
+    Viaje findByDestino(String destino);*/
+
+    @Query(value = "select user_id from usersxviajes where viaje_id = :id", nativeQuery = true)
+    List<Integer> findViajeUser(Integer id);
 }
