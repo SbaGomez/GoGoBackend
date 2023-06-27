be7bac6454b6a3870e51e91c4f3da38f3668a01f
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-06-27 19:13:21 -0300

    ViajeUserAuto y metodos sql

--- src/main/java/com/tpfinal/gogo/model/ViajeUserAuto.java
+++ src/main/java/com/tpfinal/gogo/model/ViajeUserAuto.java
@@ -3,12 +3,14 @@ package com.tpfinal.gogo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
+@Data
 @Entity
 public class ViajeUserAuto {
 
