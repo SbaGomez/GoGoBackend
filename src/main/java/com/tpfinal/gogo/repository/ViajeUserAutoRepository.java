4dec638fbdd74929410b00bdc2e0e3045fb1c89b
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-06-27 21:56:37 -0300

    format

--- src/main/java/com/tpfinal/gogo/repository/ViajeUserAutoRepository.java
+++ src/main/java/com/tpfinal/gogo/repository/ViajeUserAutoRepository.java
@@ -11,19 +11,27 @@ import java.util.List;
 public interface ViajeUserAutoRepository extends JpaRepository<ViajeUserAuto, Integer> {
 
     @Query(value =
-            "select v.*, u.nombre, u.apellido, u.edad, u.sexo, a.color, a.marca, a.modelo, a.patente from viaje v " +
-                    "INNER JOIN auto a ON a.id = v.auto_id INNER JOIN user u ON u.id = v.chofer WHERE chofer = :userId", nativeQuery = true)
+            "SELECT v.*, u.nombre, u.apellido, u.edad, u.sexo, a.color, a.marca, a.modelo, a.patente " +
+            "FROM viaje v " +
+            "INNER JOIN auto a ON a.id = v.auto_id " +
+            "INNER JOIN user u ON u.id = v.chofer " +
+            "WHERE chofer = :userId", nativeQuery = true)
     List<ViajeUserAuto> findViajesUser(Integer userId);
 
     @Query(value =
-            "select v.*, u.nombre, u.apellido, u.edad, u.sexo, a.color, a.marca, a.modelo, a.patente from viaje v " +
-                    "INNER JOIN auto a ON a.id = v.auto_id INNER JOIN user u ON u.id = v.chofer WHERE v.id = :viajeId", nativeQuery = true)
+            "SELECT v.*, u.nombre, u.apellido, u.edad, u.sexo, a.color, a.marca, a.modelo, a.patente " +
+            "FROM viaje v " +
+            "INNER JOIN auto a ON a.id = v.auto_id " +
+            "INNER JOIN user u ON u.id = v.chofer " +
+            "WHERE v.id = :viajeId", nativeQuery = true)
     ViajeUserAuto findByIdViaje(Integer viajeId);
 
     @Query(value =
-            "select v.*, u.nombre, u.apellido, u.edad, u.sexo, a.color, a.marca, a.modelo, a.patente from viaje v " +
-                    "INNER JOIN auto a ON a.id = v.auto_id INNER JOIN user u ON u.id = v.chofer " +
-                    "WHERE ubicacion_inicio = :ubicacionInicio and ubicacion_destino = :ubicacionDestino", nativeQuery = true)
+            "SELECT v.*, u.nombre, u.apellido, u.edad, u.sexo, a.color, a.marca, a.modelo, a.patente " +
+            "FROM viaje v " +
+            "INNER JOIN auto a ON a.id = v.auto_id " +
+            "INNER JOIN user u ON u.id = v.chofer " +
+            "WHERE ubicacion_inicio = :ubicacionInicio and ubicacion_destino = :ubicacionDestino", nativeQuery = true)
     List<ViajeUserAuto> findViajesUbicacion(String ubicacionInicio, String ubicacionDestino);
 
 }
