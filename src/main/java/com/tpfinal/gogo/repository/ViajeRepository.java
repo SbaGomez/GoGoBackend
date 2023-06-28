b9ad7dbcdf60ed774cbc7077a249f6b575177bf4
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-06-28 19:03:21 -0300

    findByAutoId

--- src/main/java/com/tpfinal/gogo/repository/ViajeRepository.java
+++ src/main/java/com/tpfinal/gogo/repository/ViajeRepository.java
@@ -4,7 +4,11 @@ import com.tpfinal.gogo.model.Viaje;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ViajeRepository extends JpaRepository<Viaje, Integer> {
+
+    List<Viaje> findByAutoId(int autoId);
 
 }
